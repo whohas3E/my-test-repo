1a18e101b6137bacc66b3aa54dd5f2d5a5ce25c8
whohas3E <peifenelite@gmail.com>
2021-08-23 18:08:25 +0800

    Revert "Deleted everything, we are going to need to undo"
    This reverts commit e7602f7940eff497691b9dcba7a61e4cd781dd9d.

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("This worked");
+// Working with APIs
+
+// We need to answer the following questions
+
+// What is the endpoint (URL)?
+// What query string/parameters do we need to provide?
+//  -What are the required parameters?
+//  - What optional parameters do we want to add?
+//  -What HTTP method are we using?
+// 
